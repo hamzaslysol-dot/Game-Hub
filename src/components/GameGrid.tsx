f4a62390642f88a98a3c3ebec9abfda017a1fdde
hamzaslysol-dot <hamza.slysol@gmail.com>
2025-09-18 18:53:07 +0500

    Building game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,16 +1,21 @@
-import { Text } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "./Hooks/UseGames";
+import Gamecards from "./Gamecards";
 
 const GameGrid = () => {
   const { games, error } = useGames();
   return (
     <>
       {error && <Text>{error}</Text>}
-      <ul>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        padding="10px"
+        gap={10}
+      >
         {games.map((game) => (
-          <li key={game.id}>{game.name} </li>
+          <Gamecards key={game.id} game={game} />
         ))}
-      </ul>
+      </SimpleGrid>
     </>
   );
 };
--- src/components/Hooks/UseGames.ts
+++ src/components/Hooks/UseGames.ts
@@ -2,19 +2,22 @@ import { useEffect, useState } from "react";
 import apiClient from "../../services/api-client";
 import { CanceledError } from "axios";
 
-interface Game {
-  id: number;
-  name: string;
+export interface Game
+{
+    id: number;
+    name: string;
+    background_image: string;
 }
 
-interface FetchGames {
-  count: number;
-  results: Game[];
+interface FetchGames
+{
+    count: number;
+    results: Game[];
 }
 const useGames = () =>
 {
-    const [games, setGames] = useState<Game[]>([]);
-      const [error, setError] = useState("");
+    const [ games, setGames ] = useState<Game[]>( [] );
+    const [ error, setError ] = useState( "" );
     useEffect( () =>
     {
         const controller = new AbortController();
@@ -28,9 +31,9 @@ const useGames = () =>
                 if ( err instanceof CanceledError ) return;
                 setError( err.message );
                 return () => controller.abort();
-            });
-},[] );
-    return{games, error}
+            } );
+    }, [] );
+    return { games, error };
 };
 
-    export default useGames;
+export default useGames;
