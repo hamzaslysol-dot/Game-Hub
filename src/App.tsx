3e49fb2ace9bad44a43dc4b7ba4156b1d1c9feeb
hamzaslysol-dot <hamza.slysol@gmail.com>
2025-09-19 16:09:51 +0500

    Display genres

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,16 @@ const App = () => {
         base: `"nav" " main"`,
         lg: `"nav nav" "aside main"`,
       }}
+      templateColumns={{
+        base: `'1fr'`,
+        xl: `'200px 1fr'`,
+      }}
     >
       <GridItem area="nav">
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">
+        <GridItem area="aside" paddingX={6}>
           <GenreList />
         </GridItem>
       </Show>
